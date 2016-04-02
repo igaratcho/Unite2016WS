f2c6e62f3ebe47b1fb5afbb34df1265445e38f8d
YoheiIgarashi <igaratcho@gmail.com>
2016-04-02 21:04:40 +0900

    適当な処理を実装

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -1,15 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Main : MonoBehaviour {
+public class Main : MonoBehaviour
+{
+	int clickCount=0;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 	
+	}
+
+	void OnGUI()
+	{
+		GUILayout.BeginArea(new Rect(0, 0, Screen.width, Screen.height));
+		GUILayout.FlexibleSpace();
+		GUILayout.BeginHorizontal();
+		GUILayout.FlexibleSpace();
+
+		if (GUILayout.Button (string.Format("Clicked:{0}", clickCount), GUILayout.Width(150), GUILayout.Height(150)) ) {
+			clickCount++;
+		}
+
+		GUILayout.FlexibleSpace();
+		GUILayout.EndHorizontal();
+		GUILayout.FlexibleSpace();
+		GUILayout.EndArea();
 	}
 }
